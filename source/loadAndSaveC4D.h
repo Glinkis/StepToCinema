e044dc59adf1791a14f7ec442a1a9ba3e42740f4
Victor Glindås <glinkis@gmail.com>
2017-10-29 17:02:20 +0100

    Refactoring.

---
+++ source/loadAndSaveC4D.h
@@ -0,0 +1,10 @@
+#ifndef LOAD_AND_SAVE_C4D_H__
+#define LOAD_AND_SAVE_C4D_H__
+
+#include "StepToCinema.h"
+
+using namespace melange;
+
+Bool LoadSaveC4DScene(const char *fn, const char *fnback);
+
+#endif // LOAD_AND_SAVE_C4D_H__
